2ba96f0580cb0af0ca6687affa548ee3310c68c9
donezombie <donezombie@gmail.com>
2021-10-03 02:31:48 +0700

    Remove redundant code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app";
 import { wrapper } from "redux/store";
 import "../styles/styles.scss";
-import "../styles/a.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
